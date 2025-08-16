fb07f0be74070a1ec0a23bdcf8ac13c471a45fc0
chauhan-vaun <varunchauhan097@gmail.com>
2025-08-16 14:55:37 +0530

    Adjust arrow positioning on Home page for improved layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
                   </Link>
                   
                   {/* Arrow 1 - horizontal for md screens, vertical for sm screens */}
-                  <div className="hidden md:flex items-center">
+                  <div className="hidden md:flex items-center relative" style={{ top: "-25px" }}>
                     <svg width="40" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
@@ -160,7 +160,7 @@ export default function Home() {
                   </Link>
                   
                   {/* Arrow 2 - horizontal for md screens, vertical for sm screens */}
-                  <div className="hidden md:flex items-center">
+                  <div className="hidden md:flex items-center relative" style={{ top: "-25px" }}>
                     <svg width="40" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
